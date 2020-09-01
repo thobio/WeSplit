85f80105bdbd875b62d2bfd096abe1e35a4c59ed
Thobio Joseph <jthobio2@gmail.com>
2020-09-01 05:54:54 -0700

    First Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Zerone Consulting on 01/09/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
